5bcfd945284327982de414ef95d77b1594569582
Vypsen <s.semenets2001@mail.ru>
2022-10-07 23:38:56 +1000

    add vue files

---
+++ resources/js/components/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="app">
+        <div class="header">
+            my App
+        </div>
+    <div class="content">
+        content
+    </div>
+    </div>
+</template>
+<script>
+
+</script>
+
+<style>
+
+*{
+    padding: 0px;
+    margin: 0px;
+    box-sizing: border-box;
+    background-color: white;
+}
+.header{
+    width: 100%;
+    height: 10vh;
+    background-color: #95f6a3;
+    font: normal 30px Georgia;
+    padding: 2vh 0 0 20vh;
+    box-shadow: 2px 0px 2px 0px rgba(128, 128, 128, 0.96) inset;
+}
+.header h4{
+
+}
+</style>
