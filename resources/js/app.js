180b8c819c3d4ba15270d8f170b63d9400756356
Vypsen <s.semenets2001@mail.ru>
2022-10-07 23:38:56 +1000

    add vue files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './components/App'
+
+
+const app = createApp({});
+
+app.component('hello-world', App)
+
+app.mount('#app');
