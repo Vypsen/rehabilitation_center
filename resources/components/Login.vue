aaaf0edcdbea82ddea9ff7c0e24cd064a177a6e2
Vypsen <s.semenets2001@mail.ru>
2022-10-12 23:53:15 +1000

    auth routers

--- resources/components/Login.vue
+++ resources/components/Login.vue
@@ -1,44 +1,36 @@
 <template>
-    <!--
-      This example requires updating your template:
-
-      ```
-      <html class="h-full bg-gray-50">
-      <body class="h-full">
-      ```
-    -->
-    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div class="w-full max-w-md space-y-8">
         <div>
           <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-          <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-          <p class="mt-2 text-center text-sm text-gray-600">
-            Or
-            {{ ' ' }}
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
-          </p>
+          <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Войти в свой личный кабинет</h2>
+            <p class="mt-2 text-center text-sm text-gray-600">
+                Или
+                {{ ' ' }}
+                <router-link :to="{name: 'Register'}" class="font-medium text-indigo-600 hover:text-indigo-500">
+                    Создать новый аккаунт
+                </router-link>
+            </p>
         </div>
         <form class="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" value="true" />
           <div class="-space-y-px rounded-md shadow-sm">
-            <div>
+            <div class="pb-1">
               <label for="email-address" class="sr-only">Email address</label>
-              <input id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
+              <input id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Адрес электронной почты"/>
             </div>
             <div>
               <label for="password" class="sr-only">Password</label>
-              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
+              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Пароль" />
             </div>
           </div>
 
           <div class="flex items-center justify-between">
             <div class="flex items-center">
               <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
-              <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
+              <label for="remember-me" class="ml-2 block text-sm text-gray-900">Не выходить из аккаунта</label>
             </div>
 
             <div class="text-sm">
-              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Забыли свой пароль?</a>
             </div>
           </div>
 
@@ -47,16 +39,13 @@
               <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                 <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
               </span>
-              Sign in
+              Войти
             </button>
           </div>
         </form>
-      </div>
-    </div>
   </template>
 
   <script>
-  import { LockClosedIcon } from '@heroicons/vue/20/solid'
   export default{
     name: 'Login'
   }
