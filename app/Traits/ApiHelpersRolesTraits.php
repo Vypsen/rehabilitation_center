ef2e8d29c058c3d9f05305583dd45b2417a6059d
Vypsen <s.semenets2001@mail.ru>
2022-10-28 00:11:28 +1000

    fix roles

--- app/Traits/ApiHelpersRolesTraits.php
+++ app/Traits/ApiHelpersRolesTraits.php
@@ -51,20 +51,5 @@ trait ApiHelpersRolesTraits
         ], $code);
     }
 
-    protected function postValidationRules(): array
-        {
-        return [
-            'title' => 'required|string',
-            'content' => 'required|string',
-        ];
-    }
 
-    protected function userValidatedRules(): array
-    {
-        return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ];
-    }
 }
